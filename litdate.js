3a1df3426eaf48ee0652e963fc8c6b0fd4c1fd19
xiongliding <fairplay1986@gmail.com>
2020-06-17 15:49:46 +0800

    ES6 版本，以及 npm 发布

--- litdate.js
+++ litdate.js
@@ -1,115 +1,105 @@
-(function (global, factory) {
-    typeof module !== 'undefined' && typeof module.exports !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	global.litdate = factory();
-}(this, function () { 'use strict';
-    return function (date) {
-        if (typeof date == 'undefined') {
-            date = new Date();
-        }
+module.exports = (date = new Date()) => {
+    if (typeof date == "number") {
+        date = new Date(date);
+    }
 
-        var DAY = 86400000;
+    const DAY = 86400000;
 
-        var zero = function (n) {
-            return n < 10 ? '0' + n : '' + n;
-        };
+    const zero = n => `0${n}`.substr(-2);
 
-        var weekdateoffset = function (date0101) {
-            var N = date0101.getDay();
-            var offset = N <= 4 ? N - 1 : N - 8;
-            return offset; 
-        }
-
-        var j = date.getDate();
-        var d = zero(j);
-        var w = date.getDay();
-        var N = w == 0 ? 7 : w;
-        var date0101 = new Date(date.getTime());
-        date0101.setMonth(0);
-        date0101.setDate(1);
-        var z = (date.getTime() - date0101.getTime()) / DAY;
-        var Z = z + 1;
-
-        var n = date.getMonth() + 1;
-        var m = zero(n);
-
-        var Y = date.getFullYear();
-        var y = ('' + Y).substr(2);
-        var L = (Y % 4 == 0 && Y % 100 != 0) || Y % 400 == 0 ? 1 : 0
-        var t = [31, 28 + L, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][n - 1];
-
-        var tsMonday = date.getTime() - (N - 1) * DAY;
-        var tsFirstMonday = date0101.getTime() - weekdateoffset(date0101) * DAY;
-
-        var W = (tsMonday - tsFirstMonday) / DAY / 7 + 1;
-        var o = new Date(tsMonday).getFullYear();
-        if (W == 0) {
-            var prev0101 = new Date(date0101.getTime());
-            prev0101.setYear(o);
-            var tsPrevFirstMonday = prev0101.getTime() - weekdateoffset(prev0101) * DAY;
-            W = (tsMonday - tsPrevFirstMonday) / DAY / 7 + 1;
-        } else {
-            var next0101 = new Date(Math.round(date0101.getTime() + DAY * (365 + L)));
-            var tsNextFirstMonday = next0101.getTime() - weekdateoffset(next0101) * DAY;
-            if (tsMonday >= tsNextFirstMonday) {
-                W = 1;
-                o = Y + 1;
-            }
-        }
-        var e = W;
-        W = W < 10 ? '0' + W : '' + W;
-
-        var G = date.getHours();
-        var g = G % 12 ? G % 12 : 12;
-        var H = zero(G);
-        var h = zero(g);
-        var a = G > 11 ? 'pm' : 'am';
-        var A = G > 11 ? 'PM' : 'AM';
-        var I = date.getMinutes();
-        var i = zero(I);
-        var S = date.getSeconds();
-        var s = zero(S);
-
-        var obj = {
-            d: d,
-            j: j,
-            N: N,
-            w: w,
-            z: z,
-            Z: Z,
-            W: W,
-            e: e,
-            m: m,
-            n: n,
-            t: t,
-            L: L,
-            o: o,
-            Y: Y,
-            y: y,
-            a: a,
-            A: A,
-            g: g,
-            G: G,
-            h: h,
-            H: H,
-            i: i,
-            I: I,
-            s: s,
-            S: S
-        };
-
-        var format = function (str) {
-            var chars = str.split('');
-            for (var i = 0; i < chars.length; i++) {
-                if (typeof obj[chars[i]] != 'undefined') {
-                    chars[i] = obj[chars[i]];
-                }
-            }
-            return chars.join('');
-        };
-
-        obj.format = format;
-
-        return obj;
+    const weekdateoffset = date0101 => {
+        const N = date0101.getDay();
+        const offset = N <= 4 ? N - 1 : N - 8;
+        return offset;
     };
-}));
+
+    const j = date.getDate();
+    const d = zero(j);
+    const w = date.getDay();
+    const N = w == 0 ? 7 : w;
+    let date0101 = new Date(date.getTime());
+    date0101.setMonth(0);
+    date0101.setDate(1);
+    const z = (date.getTime() - date0101.getTime()) / DAY;
+    const Z = z + 1;
+
+    const n = date.getMonth() + 1;
+    const m = zero(n);
+
+    const Y = date.getFullYear();
+    const y = `${Y}`.substr(2);
+    const L = (Y % 4 == 0 && Y % 100 != 0) || Y % 400 == 0 ? 1 : 0;
+    const t = [31, 28 + L, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][n - 1];
+
+    const tsMonday = date.getTime() - (N - 1) * DAY;
+    const tsFirstMonday = date0101.getTime() - weekdateoffset(date0101) * DAY;
+
+    let W = (tsMonday - tsFirstMonday) / DAY / 7 + 1;
+    let o = new Date(tsMonday).getFullYear();
+    if (W == 0) {
+        let prev0101 = new Date(date0101.getTime());
+        prev0101.setYear(o);
+        const tsPrevFirstMonday =
+            prev0101.getTime() - weekdateoffset(prev0101) * DAY;
+        W = (tsMonday - tsPrevFirstMonday) / DAY / 7 + 1;
+    } else {
+        const next0101 = new Date(
+            Math.round(date0101.getTime() + DAY * (365 + L))
+        );
+        const tsNextFirstMonday =
+            next0101.getTime() - weekdateoffset(next0101) * DAY;
+        if (tsMonday >= tsNextFirstMonday) {
+            W = 1;
+            o = Y + 1;
+        }
+    }
+    const e = W;
+    W = zero(W);
+
+    const G = date.getHours();
+    const g = G % 12 ? G % 12 : 12;
+    const H = zero(G);
+    const h = zero(g);
+    const a = G > 11 ? "pm" : "am";
+    const A = G > 11 ? "PM" : "AM";
+    const I = date.getMinutes();
+    const i = zero(I);
+    const S = date.getSeconds();
+    const s = zero(S);
+
+    const obj = {
+        d,
+        j,
+        N,
+        w,
+        z,
+        Z,
+        W,
+        e,
+        m,
+        n,
+        t,
+        L,
+        o,
+        Y,
+        y,
+        a,
+        A,
+        g,
+        G,
+        h,
+        H,
+        i,
+        I,
+        s,
+        S
+    };
+
+    obj.format = str =>
+        str
+            .split("")
+            .map(a => (typeof obj[a] != "undefined" ? obj[a] : a))
+            .join("");
+
+    return obj;
+};
